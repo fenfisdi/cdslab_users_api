ce0eec4dbc5dbeb2521ad587265b19f12b53ec65
David Peña <dpenah@intergrupo.com>
2021-04-19 09:28:10 -0500

    Update: Update Singleton in Database

--- src/api.py
+++ src/api.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
 from src.config import fastApiConfig
-from src.db import create_connection
+from src.db import MongoEngine
 from src.routes import user_routes, credential_routes
 
 app = FastAPI(**fastApiConfig)
-db = create_connection()
+db = MongoEngine().get_connection()
 
 app.include_router(user_routes)
 app.include_router(credential_routes)
